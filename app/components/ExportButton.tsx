8ad5637e5ef117da164485f301ad674728815131
Marvin Tagolimot Jr. <31147592+diffident016@users.noreply.github.com>
2025-04-25 18:02:55 +0800

    feat: insight report template

--- app/components/ExportButton.tsx
+++ app/components/ExportButton.tsx
@@ -1,25 +1,38 @@
 "use client";
-import { useState } from "react";
-import { MdDownload, MdInsights } from "react-icons/md";
+import { MdInsights } from "react-icons/md";
+import { HTMLTemplate } from "./utils/InsightReportTemplate";
+import { generateInsights } from "./utils/generateInsights";
+import printJS from "print-js";
 
-const ExportButton = ({ onExportPDF }: { onExportPDF: () => void }) => {
-  const [isOpen, setIsOpen] = useState(false);
+const ExportButton = () => {
+  const generateReport = async () => {
+    const insights = await generateInsights();
+    const htmlString = HTMLTemplate({ insights, printJS });
+
+    // console.log(insights);
+
+    const reportWindow = window.open("insight-report", "_blank");
+    if (reportWindow) {
+      reportWindow.document.open();
+      reportWindow.document.write(htmlString);
+      reportWindow.document.close();
+    }
+  };
 
   return (
-    <div className="relative">
-      <button
+    <div>
+      <a
         onClick={() => {
-          console.log("Generated Insights!");
-          onExportPDF();
-          setIsOpen(false);
+          generateReport();
         }}
-        className="flex flex-row items-center justify-center rounded-md px-4 py-2 bg-[var(--brightaqua)] dark:bg-[var(--brimagenta)] transform transition duration-500 hover:scale-110"
       >
-        <span className="text-[var(--bluetext)] dark:text-white text-xs font-semibold dark:font-medium mr-2">
-          Generate Insights
-        </span>
-        <MdInsights size={16} />
-      </button>
+        <button className="flex flex-row items-center justify-center rounded-md px-4 py-2 bg-[var(--brightaqua)] dark:bg-[var(--brimagenta)] transform transition duration-500 hover:scale-110">
+          <span className="text-white text-xs font-semibold dark:font-medium mr-2">
+            Generate Insights
+          </span>
+          <MdInsights size={16} />
+        </button>
+      </a>
     </div>
   );
 };
